8ed7e30d5b61163bea93e738c02883c1b66ceee4
Ahmed Nasr <nasr@certoclav.com>
2022-05-03 10:14:56 +0200

    added base backend folder

---
+++ backend/src/utils/hash.js
@@ -0,0 +1,56 @@
+const crypto = require("crypto");
+const jwt = require("jsonwebtoken")
+
+function hash(input) {
+    return crypto.createHash('sha256').update(input).digest('hex');
+}
+
+function createRandomSalt() {
+    return crypto.randomBytes(64).toString('hex');
+}
+
+function createPasswordHash(password, salt) {
+    return hash(password + salt)
+}
+
+function createToken(user) {
+    const TEN_MINUTES = 60 * 10 // 60seconds x 10
+    const initiatedAt = Math.floor(Date.now() / 1000)
+    const expiresAt = initiatedAt + TEN_MINUTES
+
+    const tokenPayload = {
+        sub: user._id, // subjekt
+        tokenType: "access",
+        iat: initiatedAt, // initiaed at in seconds
+        exp: expiresAt    // expires
+    }
+
+    const token = jwt.sign(tokenPayload, process.env.JWT_SECRET)
+    return token
+}
+
+function imageBufferToBase64(imgBuffer, mimeType) {
+    return "data:" + mimeType + ";base64," + imgBuffer.toString('base64')
+}
+
+module.exports = {
+    hash,
+    createRandomSalt,
+    createPasswordHash,
+    createToken,
+    imageBufferToBase64
+}
+
+// Rainbow Table: Hash <--> Passwort <--> Hash Algo
+// Hallo + ???
+// 753692ec36adb4c794c973945eb2a99c1649703ea6f76bf259abb4fb838e013e
+
+// x09309309ü13i13üi1  <-- Hallo1309üj1093rj0ü913jr
+// 9835u9p24up9813u91  <-- Hallo092409498u24pui2p33
+
+// Hallo Welt!
+// a582e8c28249fe7d7990bfa0afebd2da9185a9f831d4215b4efec74f355b301a
+
+// 1. Besonderheit: Einwegfunktion aka. Umkehrfunktion ist schwer zu berechenen
+// 2. Besonderheit: Gleicher input --> gleicher output
+// 3. Besonderheit: Chaos --> minimale Änderung im Input --> fatalen Effekt/Änderung im Output
