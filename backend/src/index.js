b1df05e4e2d4feca2f0da42aabb3064b1070d722
Ahmed Nasr <nasr@certoclav.com>
2022-05-04 09:57:02 +0200

    added facade layer with express-validator

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,7 +1,10 @@
 const express = require("express")
 const cors = require("cors")
 const morgan = require("morgan")
-const { UserService } = require("./use-cases")
+const { body, validationResult } = require('express-validator');
+
+const { UserService } = require("./use-cases");
+const { doValidations } = require("./facade/doValidations");
 
 const PORT = process.env.PORT || 9000
 const app = express()
@@ -27,30 +30,43 @@ app.get("/api/users/all", async (_, res) => {
     }
 })
 
-app.post("/api/users/register", async (req, res) => {
-    try {
-        const userInfo = req.body
-        const result = await UserService.registerUser(userInfo)
+app.post("/api/users/register",
+    // facade layer
+    body("username").isLength({ min: 1, max: 25 }),
+    body("email").isEmail(),
+    body("password").isStrongPassword(),
+    doValidations,
+    async (req, res) => {
+        try {
+            const userInfo = req.body
+            const result = await UserService.registerUser(userInfo)
 
-        res.status(201).json(result) // 201 ==> 'Created'
-    } catch (err) {
-        console.log(err)
-        res.status(500).json({ err: { message: err.message } })
+            res.status(201).json(result) // 201 ==> 'Created'
+        } catch (err) {
+            console.log(err)
+            res.status(500).json({ err: { message: err.message } })
+        }
     }
-})
+)
 
-app.post("/api/users/verifyEmail", async (req, res) => {
-    try {
-        const email = req.body.email
-        const sixDigitCode = req.body.sixDigitCode
-        const result = await UserService.verifyUserEmail({ email, sixDigitCode })
+app.post("/api/users/verifyEmail", 
+    // facade layer
+    body("email").isEmail(),
+    body("sixDigitCode").isLength({ min: 6 }),
+    doValidations,
+    async (req, res) => {
+        try {
+            const email = req.body.email
+            const sixDigitCode = req.body.sixDigitCode
+            const result = await UserService.verifyUserEmail({ email, sixDigitCode })
 
-        res.status(200).json(result)
-    } catch (err) {
-        console.log(err)
-        res.status(500).json({ err: { message: err.message } })
+            res.status(200).json(result)
+        } catch (err) {
+            console.log(err)
+            res.status(500).json({ err: { message: err.message } })
+        }
     }
-})
+)
 
 app.listen(PORT, () => console.log("Server ready at", PORT))
 
